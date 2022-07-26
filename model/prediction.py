1676ca017a078f2a5c72c659dbeae6166d8a4574
Shehroz Sohail <shz.soh@gmail.com>
2022-07-26 15:44:09 -0700

    modified:   model/prediction.py 	deleted:    src/__pycache__/main.cpython-310.pyc 	deleted:    src/__pycache__/prediction.cpython-310.pyc 	modified:   src/prediction.py

--- model/prediction.py
+++ model/prediction.py
@@ -19,41 +19,56 @@ class predicter:
         self.load_path = load_path
         self.model = self.load_model()
 
-    def load_model(self):
+    def load_model(self, from_local: bool = False):
         """
         loads model from either local repository or from hugging face pipeline
+        from_local: bool
+            loads model locally if true. else loads transformer pipeline
         """
-        # script for when model is loaded from storage
-        # model = (AutoModelForSequenceClassification
-        # .from_pretrained(self.model_ckpt, num_labels=2))
-        # checkpoint=torch.load(self.load_path,
-        # map_location=torch.device('cpu'))
-        # self.new_model=model.load_state_dict(checkpoint)
-
-        model_id = "shozi218/finetuned-emotion-model"
-        model = pipeline("text-classification", model=model_id)
+        if from_local == True:
+            model = AutoModelForSequenceClassification.from_pretrained(
+                self.model_ckpt, num_labels=2
+            )
+            checkpoint = torch.load(self.load_path, map_location=torch.device("cpu"))
+            self.new_model = model.load_state_dict(checkpoint)
+        else:
+            model_id = "shozi218/finetuned-emotion-model"
+            model = pipeline("text-classification", model=model_id)
         return model
 
-    def predict(self, text: str) -> int:
+    def predict(self, text: str, from_local: bool = False) -> int:
         """
         creates prediction from either transformer pipeline or locally loaded model
 
         Attributes:
         ----------
         text: str
-        input text/tweet for sentiment classification
+            input text/tweet for sentiment classification
+        from_local: bool
+            loads model locally if true. else loads transformer pipeline
         """
-        # script for when model is loaded from storage
-        # encode_text=self.tokenizer(text, padding=True, truncation=True, return_tensors="pt")
-        # output=self.model(input_ids=encode_text['input_ids'], attention_mask=encode_text['attention_mask'])
-        # probabilities= torch.nn.functional.softmax(output.logits, dim=1).flatten().detach().numpy().tolist()
-        # probabilities=dict(zip(self.data_labels, probabilities))
-        # final_emotion=self.data_labels[torch.argmax(output[0])]
-
-        preds = self.model(text, return_all_scores=True)
-        prediction_probs = [preds[i]["score"] for i in range(len(preds))]
-        final_emotion = self.data_labels[np.argmax(prediction_probs)]
-        probabilities = dict(zip(self.data_labels, prediction_probs))
+        if from_local == True:
+            encode_text = self.tokenizer(
+                text, padding=True, truncation=True, return_tensors="pt"
+            )
+            output = self.model(
+                input_ids=encode_text["input_ids"],
+                attention_mask=encode_text["attention_mask"],
+            )
+            probabilities = (
+                torch.nn.functional.softmax(output.logits, dim=1)
+                .flatten()
+                .detach()
+                .numpy()
+                .tolist()
+            )
+            probabilities = dict(zip(self.data_labels, probabilities))
+            final_emotion = self.data_labels[torch.argmax(output[0])]
+        else:
+            preds = self.model(text, return_all_scores=True)
+            prediction_probs = [preds[i]["score"] for i in range(len(preds))]
+            final_emotion = self.data_labels[np.argmax(prediction_probs)]
+            probabilities = dict(zip(self.data_labels, prediction_probs))
         return final_emotion, probabilities
 
 
--- src/prediction.py
+++ src/prediction.py
@@ -1,5 +1,6 @@
 import numpy as np
-from transformers import AutoTokenizer, pipeline
+import torch
+from transformers import AutoModelForSequenceClassification, AutoTokenizer, pipeline
 
 
 class predicter:
@@ -18,30 +19,56 @@ class predicter:
         self.load_path = load_path
         self.model = self.load_model()
 
-    def load_model(self):
-        # script for when model is loaded from storage
-        # model = (AutoModelForSequenceClassification
-        # .from_pretrained(self.model_ckpt, num_labels=2))
-        # checkpoint=torch.load(self.load_path,
-        # map_location=torch.device('cpu'))
-        # self.new_model=model.load_state_dict(checkpoint)
-
-        model_id = "shozi218/finetuned-emotion-model"
-        model = pipeline("text-classification", model=model_id)
+    def load_model(self, from_local: bool = False):
+        """
+        loads model from either local repository or from hugging face pipeline
+        from_local: bool
+            loads model locally if true. else loads transformer pipeline
+        """
+        if from_local == True:
+            model = AutoModelForSequenceClassification.from_pretrained(
+                self.model_ckpt, num_labels=2
+            )
+            checkpoint = torch.load(self.load_path, map_location=torch.device("cpu"))
+            self.new_model = model.load_state_dict(checkpoint)
+        else:
+            model_id = "shozi218/finetuned-emotion-model"
+            model = pipeline("text-classification", model=model_id)
         return model
 
-    def predict(self, text: str) -> int:
-        # script for when model is loaded from storage
-        # encode_text=self.tokenizer(text, padding=True, truncation=True, return_tensors="pt")
-        # output=self.model(input_ids=encode_text['input_ids'], attention_mask=encode_text['attention_mask'])
-        # probabilities= torch.nn.functional.softmax(output.logits, dim=1).flatten().detach().numpy().tolist()
-        # probabilities=dict(zip(self.data_labels, probabilities))
-        # final_emotion=self.data_labels[torch.argmax(output[0])]
+    def predict(self, text: str, from_local: bool = False) -> int:
+        """
+        creates prediction from either transformer pipeline or locally loaded model
 
-        preds = self.model(text, return_all_scores=True)
-        prediction_probs = [preds[i]["score"] for i in range(len(preds))]
-        final_emotion = self.data_labels[np.argmax(prediction_probs)]
-        probabilities = dict(zip(self.data_labels, prediction_probs))
+        Attributes:
+        ----------
+        text: str
+            input text/tweet for sentiment classification
+        from_local: bool
+            loads model locally if true. else loads transformer pipeline
+        """
+        if from_local == True:
+            encode_text = self.tokenizer(
+                text, padding=True, truncation=True, return_tensors="pt"
+            )
+            output = self.model(
+                input_ids=encode_text["input_ids"],
+                attention_mask=encode_text["attention_mask"],
+            )
+            probabilities = (
+                torch.nn.functional.softmax(output.logits, dim=1)
+                .flatten()
+                .detach()
+                .numpy()
+                .tolist()
+            )
+            probabilities = dict(zip(self.data_labels, probabilities))
+            final_emotion = self.data_labels[torch.argmax(output[0])]
+        else:
+            preds = self.model(text, return_all_scores=True)
+            prediction_probs = [preds[i]["score"] for i in range(len(preds))]
+            final_emotion = self.data_labels[np.argmax(prediction_probs)]
+            probabilities = dict(zip(self.data_labels, prediction_probs))
         return final_emotion, probabilities
 
 
